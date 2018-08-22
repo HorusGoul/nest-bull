bce27994d432d7cc443c7bedff94a83d2984f07d
Horus Lugo <horusgoul@gmail.com>
2018-08-23 00:13:10 +0200

    Build previous commit.

--- dist/bull.providers.js
+++ dist/bull.providers.js
@@ -8,18 +8,20 @@ function createQueues(options) {
         provide: bull_utils_1.getQueueToken(option.name),
         useFactory: () => {
             const queue = new Bull(option.name ? option.name : 'default', option.options);
-            option.processors.forEach((processor) => {
-                if (bull_types_1.isAdvancedProcessor(processor)) {
-                    const hasName = !!processor.name;
-                    const hasConcurrency = !!processor.concurrency;
-                    hasName && hasConcurrency ? queue.process(processor.name, processor.concurrency, processor.callback)
-                        : hasName ? queue.process(processor.name, processor.callback)
-                            : queue.process(processor.concurrency, processor.callback);
-                }
-                else {
-                    queue.process(processor);
-                }
-            });
+            if (option.processors) {
+                option.processors.forEach((processor) => {
+                    if (bull_types_1.isAdvancedProcessor(processor)) {
+                        const hasName = !!processor.name;
+                        const hasConcurrency = !!processor.concurrency;
+                        hasName && hasConcurrency ? queue.process(processor.name, processor.concurrency, processor.callback)
+                            : hasName ? queue.process(processor.name, processor.callback)
+                                : queue.process(processor.concurrency, processor.callback);
+                    }
+                    else {
+                        queue.process(processor);
+                    }
+                });
+            }
             return queue;
         },
     }));
